6a2ec1bbed13ed24d3445624371d6a858debe601
T5wRzOtsUndPho <T5wRzOtsUndPho@mail.ru>
2019-01-14 23:45:19 +0300

    нарисовал левую и правую панели. пока не доделано.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -63,12 +63,18 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
+    <div class="left_panel">
+        .left_panel
+    </div>
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
+    </div>
+    <div class="right_panel">
+        .right_panel
     </div>
 </div>
 
