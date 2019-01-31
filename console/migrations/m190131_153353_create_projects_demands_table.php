18064fba058269cc233db44ee9906320d9cab74b
T5wRzOtsUndPho <T5wRzOtsUndPho@mail.ru>
2019-01-31 23:33:30 +0300

    поправил миграцию

--- console/migrations/m190131_153353_create_projects_demands_table.php
+++ console/migrations/m190131_153353_create_projects_demands_table.php
@@ -13,16 +13,18 @@ class m190131_153353_create_projects_demands_table extends Migration
     public function safeUp()
     {
         $this->createTable('projects_demands', [
-            'id' => $this->primaryKey(),
+            //'id' => $this->primaryKey(),
             'fk_project' => $this->integer()->notNull()->comment('Проект'),
             'fk_demand' => $this->integer()->notNull()->comment('Требование'),
-            'is_relevant' => $this->integer()->notNull()->comment('Актуальность')->defaultValue(0),
-            'created_by'=>$this->integer()->notNull(),
-            'updated_by'=>$this->integer(),
-            'created_at'=>$this->integer()->notNull(),
-            'updated_at'=>$this->integer(),
-            'comment'=>$this->string(255)->notNull()->comment('Комментарий'),
+            'is_relevant' => $this->integer()->notNull()->comment('Актуальность')->defaultValue(1),
+            'created_by' => $this->integer()->notNull(),
+            'updated_by' => $this->integer(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer(),
+            'comment' => $this->string(255)->comment('Комментарий'),
         ]);
+
+        $this->addPrimaryKey('pk', 'projects_demands', ['fk_project', 'fk_demand']);
 
         $this->addForeignKey('pd-projects', 'projects_demands', 'fk_project', 'project', 'id');
         $this->addForeignKey('pd-demands', 'projects_demands', 'fk_demand', 'demands', 'id');
@@ -35,10 +37,11 @@ class m190131_153353_create_projects_demands_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropForeignKey('pd-projects','projects_demands');
-        $this->dropForeignKey('pd-demands','projects_demands');
-        $this->dropForeignKey('pd-user-created_by','projects_demands');
-        $this->dropForeignKey('pd-user-updated_by','projects_demands');
+        $this->dropForeignKey('pd-projects', 'projects_demands');
+        $this->dropForeignKey('pd-demands', 'projects_demands');
+        $this->dropForeignKey('pd-user-created_by', 'projects_demands');
+        $this->dropForeignKey('pd-user-updated_by', 'projects_demands');
+        $this->dropPrimaryKey('pk', 'projects_demands');
 
         $this->dropTable('projects_demands');
     }
